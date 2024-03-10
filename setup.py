b6a32296241e13a24e4a4aa2f9206c60393dd9aa
siddharth.shorya <siddharth.shorya@mu-sigma.com>
2024-03-10 23:12:38 +0530

    docs: version change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from  setuptools import setup, find_packages
 
 setup(
     name='dse2e',
-    version='0.0.2',
+    version='0.0.3',
     description='A package which contains all functions which are required by a practisioneer to perform industry specific tasks will a single line of code (WIP)',
     author=["Udai Agarwal", "Siddharth Shorya"],
     author_email=["udaiag@gmail.com", "shoryasiddharth90@gmail.com"],
