908ac51f7fc0c0764b1ab6ae2b642bce573a96de
siddharth.shorya <siddharth.shorya@mu-sigma.com>
2024-03-10 23:28:41 +0530

    docs: added authors fix and url

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from  setuptools import setup, find_packages
 
 setup(
     name='dse2e',
-    version='0.0.4',
+    version='0.0.5',
     description='A package which contains all functions which are required by a practisioneer to perform industry specific tasks will a single line of code (WIP)',
     author= "Udai Agarwal, Siddharth Shorya",
     author_email="shoryasiddharth90@gmail.com",
