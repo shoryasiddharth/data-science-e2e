903ea046e2c3165d8f19485d7e19cfba58a1daaf
siddharth.shorya <siddharth.shorya@mu-sigma.com>
2024-03-10 15:48:38 +0530

    created basic structure for pypi push

---
+++ app.py
@@ -0,0 +1,2 @@
+from dse2e import add
+print(add(100,1))
---
+++ setup.py
@@ -0,0 +1,14 @@
+from  setuptools import setup, find_packages
+
+setup(
+    name='dse2e',
+    version='0.0.1',
+    description='A package which contains all functions which are required by a practisioneer to perform industry specific tasks will a single line of code (WIP)',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',
+)
