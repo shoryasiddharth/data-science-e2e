874c0e1a09088ebce786c7227b7e0290ea4bda6c
siddharth.shorya <siddharth.shorya@mu-sigma.com>
2024-03-10 23:20:50 +0530

    docs: added authors fix

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from  setuptools import setup, find_packages
 
 setup(
     name='dse2e',
-    version='0.0.3',
+    version='0.0.4',
     description='A package which contains all functions which are required by a practisioneer to perform industry specific tasks will a single line of code (WIP)',
-    author= "Udai Agarwal, Siddharth Shorya",
-    author_email="udaiag@gmail.com, shoryasiddharth90@gmail.com",
-    long_description=open('README.md').read(),
+    author= ["Udai Agarwal, Siddharth Shorya"],
+    author_email="shoryasiddharth90@gmail.com",
+    long_description=open('README.md').read() + "\n\nAuthors:\n - Author One <author.one@example.com>\n - Author Two <author.two@example.com>",
     long_description_content_type="text/markdown",
     url="https://github.com/yourusername/your_package_name",
     packages=find_packages(),
