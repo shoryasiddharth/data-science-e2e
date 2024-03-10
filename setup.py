9139c317d1613d5c77173a7edbf766ad0e7e5e31
siddharth.shorya <siddharth.shorya@mu-sigma.com>
2024-03-10 23:33:12 +0530

    docs: pypi documents update

--- setup.py
+++ setup.py
@@ -6,9 +6,14 @@ setup(
     description='A package which contains all functions which are required by a practisioneer to perform industry specific tasks will a single line of code (WIP)',
     author= "Udai Agarwal, Siddharth Shorya",
     author_email="shoryasiddharth90@gmail.com",
-    long_description=open('README.md').read() + "\n\nAuthors:\n - Author One <author.one@example.com>\n - Author Two <author.two@example.com>",
+    long_description=open('README.md').read() + "\n\nAuthors:\n - Udai Agarwal <udai.ag@gmail.com>\n - Siddharth Shorya <shoryasiddharth90@gmail.com>",
     long_description_content_type="text/markdown",
     url="https://github.com/shoryasiddharth/data-science-e2e",
+    PROJECT_URLS = {
+        "Bug Tracker": "https://github.com/shoryasiddharth/data-science-e2e/issues",
+        "Documentation": "https://github.com/shoryasiddharth/data-science-e2e",
+        "Source Code": "https://github.com/shoryasiddharth/data-science-e2e",
+    },
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
